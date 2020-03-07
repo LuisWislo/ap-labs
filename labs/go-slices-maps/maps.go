8ea23d78b28aa4b78c9f614de3d1e27318cdbc5b
Luis Wilson <luisfwilson1998@gmail.com>
2020-03-07 01:52:48 -0600

    solve slices and maps
    Signed-off-by: Luis Wilson <luisfwilson1998@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,29 @@
 package main
 
-func main() {
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	output := make(map[string] int)
+	words := strings.Fields(s)
+	
+	for _, w := range words {
+		_,exists := output[w] 
+		
+		if exists {
+			output[w] = output[w] +1
+		} else {
+			output[w] = 1
+		}
+		
+	}
+		
+	return output
 }
+
+func main() {
+	wc.Test(WordCount)
+}
+
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,22 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	a := make([][]uint8, dy)
+
+	for i := range a {
+		a[i] = make([]uint8, dx)
+	}
+
+	for y := 0; y < dy; y++ {
+		for x := 0; x < dx; x++ {
+			a[y][x] = uint8(x ^ y)
+		}
+	}
+	return a
+}
+
 func main() {
+	pic.Show(Pic)
 }
