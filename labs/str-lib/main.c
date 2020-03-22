fc678a0bb27ea6e3965a827688cb5d39a8248671
Luis Wilson <luisfwilson1998@gmail.com>
2020-03-21 21:39:21 -0600

    solve string manipulation library
    Signed-off-by: Luis Wilson <luisfwilson1998@gmail.com>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
-int main() {
+//Luis Wilson A00226649
+
+int mystrlen(char*);
+char *mystradd(char*, char*);
+int mystrfind(char*, char*);
+
+int main(int argc, char **argv) {
+
+    if(argc != 4){
+        printf("Unsupported number of arguments. Usage: ./main.o arg0 arg1 arg2");
+    } else {
+        int initialLength = mystrlen(argv[1]);
+        char *concatenated = mystradd(argv[1], argv[2]);
+        char *foundSub = mystrfind(concatenated, argv[3]) ? "yes" : "no";
+        printf("Initial Length      : %d\n", initialLength);
+        printf("New String          : %s\n", concatenated);
+        printf("SubString was found : %s\n", foundSub);
+    }
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,62 @@
-
-
+//Luis Wilson A00226649
 int mystrlen(char *str){
-    return 0;
+    char c;
+    int i=0;
+    while(c = str[i] != '\0'){
+       i++; 
+    }
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+
+    char *final = origin;
+
+    while(*origin != '\0'){
+        origin++;
+    }
+
+    while(*addition != '\0'){
+        *origin++ = *addition++;
+    }
+
+    *origin = '\0';
+
+    return final;
 }
 
 int mystrfind(char *origin, char *substr){
+
+    int i=0, j=0,k;
+    int length = mystrlen(origin);
+    int subLength = mystrlen(substr);
+
+    if(subLength == 0)
+        return 1;
+    if(length == 0)   
+        return 0;
+    if(subLength > length)
+        return 0;
+    
+    while(i < length){
+        if(origin[i++] == substr[0]){
+            j=i;
+            k=1;
+            int canReturn = 1;
+            
+            while(k < subLength){
+                if(j<length){
+                    if(origin[j++] != substr[k++]){
+                        canReturn = 0;
+                        break;        
+                    }
+                } else {
+                    return 0;
+                }
+            }
+            if(canReturn)
+                return 1;
+        }
+    }
     return 0;
 }
