221afd32a9644ca0c9f7cf8486b90a6d1f91a318
Luis Wilson <luisfwilson1998@gmail.com>
2020-05-14 19:36:53 -0500

    progress

--- labs/file-dir-monitor/logger.c
+++ labs/file-dir-monitor/logger.c
@@ -1,23 +1,103 @@
 #include <stdio.h>
 #include "logger.h"
+#include<stdarg.h>		
+#include<string.h>
+#include<stdlib.h>
+#include<signal.h>
+ #include <syslog.h>
+
+int is_stdout = 1;
+
+//Luis Wilson A00226649
 
 int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
+
+    char color[13];
+    sprintf(color, "%c[%d;%d;%dm", 0x1B, 0, 37, 40); ///white over black
+    printf("%s", color);
+
+    if(strcmp(logType, "stdout") == 0) {
+        //printf("\nInitializing Logger on: %s\n", logType);
+        is_stdout = 1;
+    } else if(strcmp(logType, "syslog") == 0) {
+        //printf("\nInitializing Logger on: %s\n", logType);
+        is_stdout = 0;
+    } else {
+        printf("\nInvalid init option %s. Please input 'stdout' or 'syslog'\n", logType);
+    }
+
     return 0;
 }
 
 int infof(const char *format, ...) {
-    return 0;
+    int r = 0;
+    char color[13];
+    sprintf(color, "%c[%d;%d;%dm", 0x1B, 0, 36, 40); ///cyan over black
+    printf("%s", color);
+    va_list args;
+    va_start(args, format);
+
+    if(is_stdout) {
+        r = vprintf(format, args);
+    } else {
+        vsyslog(LOG_INFO, format, args);
+    }
+    
+    va_end(args);
+    return r;
 }
 
 int warnf(const char *format, ...) {
-    return 0;
+    int r = 0;
+    char color[13];
+    sprintf(color, "%c[%d;%d;%dm", 0x1B, 0, 33, 40); // yellow over black
+    printf("%s", color);
+    va_list args;
+    va_start(args, format);
+
+    if(is_stdout) {
+        r = vprintf(format, args);
+    } else {
+        vsyslog(LOG_WARNING, format, args);
+    }
+
+    va_end (args);
+    return r;
 }
 
 int errorf(const char *format, ...) {
-    return 0;
-}
-int panicf(const char *format, ...) {
-    return 0;
+    int r = 0;
+    char color[13];
+    sprintf(color, "%c[%d;%d;%dm", 0x1B, 0, 35, 40); // magenta over black
+    printf("%s", color);
+    va_list args;
+    va_start(args, format);
+    
+    if(is_stdout) {
+        r = vprintf(format, args);
+    } else {
+        vsyslog(LOG_ERR ,format, args);
+    }
+
+    va_end (args);
+    return r;
 }
 
+int panicf(const char *format, ...) {
+    int r = 0;
+    char color[13];
+    sprintf(color, "%c[%d;%d;%dm", 0x1B, 0, 31, 40); // red over black
+    printf("%s", color);
+    va_list args;
+    va_start(args, format);
+    
+    if(is_stdout) {
+        r = vprintf(format, args);
+    } else {
+        vsyslog(LOG_EMERG, format, args);
+    }
+
+    va_end (args);
+    return raise (SIGABRT);
+    //return r;
+}
--- labs/file-dir-monitor/monitor.c
+++ labs/file-dir-monitor/monitor.c
@@ -1,7 +1,86 @@
+#define _XOPEN_SOURCE 500
 #include <stdio.h>
 #include "logger.h"
+#include <sys/inotify.h>
+#include <unistd.h>
+#include <ftw.h>
+#define NO_EVENTS 1024
+#define EVENT_SIZE (sizeof (struct inotify_event))
+#define LEN_NAME 20
+#define BUFFER_SIZE (NO_EVENTS * (EVENT_SIZE + LEN_NAME))
 
-int main(){
-    // Place your magic here
+int fd;
+
+int nftw_function(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
+    int watch;
+
+    watch = inotify_add_watch(fd, fpath, IN_CREATE | IN_MODIFY | IN_DELETE);
+
+    /*if(watch < 0) {
+        errorf("Unable to watch directory\n");
+        return -1;
+    }*/
+
+    return 0;
+}
+
+
+int main(int argc, char** argv){
+    initLogger("stdout");
+
+    if (argc != 2) {
+        warnf("Unsupported number of arguments.\n");
+        infof("Usage: ./monitor </directory>\n");
+        return -1;
+    }
+
+    char *directory = argv[1];
+
+    int i;
+    char buffer[4096];
+
+    fd = inotify_init();
+
+    if(fd < 0) {
+        errorf("Error initializing inotify.\n");
+        return -1;
+    }
+
+    int traversal;
+    traversal = nftw(directory, nftw_function, 30, FTW_PHYS);
+
+    if(traversal < 0) {
+        errorf("Error traversing directories.\n");
+        return -1;
+
+    }
+
+    while(1) {
+        
+        int j = 0;
+        int readBytes = read(fd, buffer, sizeof(buffer));
+
+        if(readBytes < 0) {
+            errorf("Unable to read file descriptor\n");
+            return -1;
+        }
+
+        while (j < readBytes) {
+            struct inotify_event *ev = (struct inotify_event *) buffer;
+
+            if(ev->mask & IN_CREATE) {
+                infof("Created %s!\n",ev->name);
+            } else if(ev->mask & IN_MODIFY) {
+                infof("Modified %s!\n",ev->name);
+            } else if(ev->mask & IN_DELETE) {
+                infof("Deleted %s!\n",ev->name);
+            }
+
+            j+= EVENT_SIZE + ev->len;
+
+        }
+
+    }
+
     return 0;
 }
